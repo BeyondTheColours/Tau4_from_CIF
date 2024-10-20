e7357332aa2a740d239f950e9bf8656b9e0434ae
BeyondTheColours <86986714+BeyondTheColours@users.noreply.github.com>
2024-10-20 14:46:34 +0100

    Updated Tau4_from_cif
    Improved control flow in the program to return 'None' from functions which detect problems the input data.
    Added separate functions to provide explanations of the problems encountered when a 'None' value is returned with explicit checks None values at logical points in the program.
    Current state :
    - working for valid CIF
    - fails correctly for invalid file input
    - fails correctly for no central atom provided
    
    Need to test for:
    - Central atom no valid for Tau4 calc but is in the CIF
    
    Need to improve:
    - Parsing for central atom and the atoms it is bonded to. Currently works for my own CIFs but should be generalised using actual regex rather than relying so heavily on the current assumptions of the formatting in the CIF (e.g. the way atoms are name etc.)

--- Tau4_from_cif.py
+++ Tau4_from_cif.py
@@ -1,105 +1,122 @@
 import sys
 import math
+import re
 
 def get_angle_lines(cif_file_text, atom):
 
-	angles_text = cif_file_text.split("_geom_angle_publ_flag\n")[1].split("loop_")[0]
-	angles_text_list = angles_text.split("\n")
+    angles_text = cif_file_text.split("_geom_angle_publ_flag\n")[1].split("loop_")[0]
+    angles_text_list = angles_text.split("\n")
 
-	angles_text_list_cleaned = []
-	for i in angles_text_list:
-		temp = ""
-		if i.split(" ")[0] == "":
-			for j in i.split(" "):
-				if j != "":
-					temp += j + " "
-			angles_text_list_cleaned.append(temp)
-		else:
-			angles_text_list_cleaned.append(i)
+    angles_text_list_cleaned = []
+    for i in angles_text_list:
+        temp = ""
+        if i.split(" ")[0] == "":
+            for j in i.split(" "):
+                if j != "":
+                    temp += j + " "
+            angles_text_list_cleaned.append(temp)
+        else:
+            angles_text_list_cleaned.append(i)
 
-	angles_at_atom = []
-	for i in angles_text_list_cleaned:
-		temp = i.split(" ")
-		try:
-			if temp[1] == atom:
-				angles_at_atom.append(temp)
-		except IndexError:
-			pass
+    angles_at_atom = []
+    for i in angles_text_list_cleaned:
+        temp = i.split(" ")
+        try:
+            if temp[1] == atom:
+                angles_at_atom.append(temp)
+        except IndexError:
+            pass
 
-	if len(angles_at_atom) > 6:
-		return f"\nError - more than six angles found around {atom}. Cannot compute Tau4."
-	if 6 > len(angles_at_atom) > 0:
-		pluraliser = 0
-		if len(angles_at_atom) > 1:
-			pluraliser = 1
-		return f"\nError - only {len(angles_at_atom)} angle{pluraliser*'s'} found around \"{atom}\". Cannot compute Tau4."
-	if len(angles_at_atom) == 0:
-		return f"\nError - No angles around \"{atom}\" were found."
-		
-	return angles_at_atom
+    return angles_at_atom
 
-def get_angle_values(angles_line_list):
-	res = []
-	for i in angles_line_list:
-		temp = i[3].split("(")
-		val = float(temp[0])
-		res.append(val)
+def validate_angle_line_list(angle_line_list):
+    if len(angle_line_list) == 6:
+        return angle_line_list
+    return None
 
-	return res
+def explain_angle_list_issue(angle_line_list, central_atom):
+    l = len(angle_line_list)
+    pluraliser = 0
+    if 6 > l > 0:
+        if l > 1:
+            pluraliser = 1
+        return f"Only {l} angle{pluraliser*'s'} found at {central_atom}.\nFor a four-coordinate atom there should be six.\nMake sure the correct central atom was provided!"
+    elif l == 0:
+        return f"No angles were found at {central_atom}."
+    else:
+        return f"Too many angles ({l}) were found at {central_atom}.\nFor a four-coordinate atom there should be six.\nMake sure the correct central atom was provided!"
 
+def get_angle_values(angle_line_list):
+    res = []
+    for i in angle_line_list:
+        temp = i[3].split("(")
+        val = float(temp[0])
+        res.append(val)
 
+    return res
+
+    
 def get_largest_2_angles(angles_list):
-	largest = 0
-	second_largest = 0
-	for i in angles_list:
-		if i >= second_largest:
-			if i <= largest:
-				second_largest = i
-			else:
-				second_largest = largest
-				largest = i
-
-	return [largest, second_largest]
-			
+    largest = 0
+    second_largest = 0
+    for i in angles_list:
+        if i >= second_largest:
+            if i <= largest:
+                second_largest = i
+            else:
+                second_largest = largest
+                largest = i
+    return [largest, second_largest]
+            
 def calc_tau4(a,b):
-	theta = math.acos(-1/3)*(180/math.pi)
-	top = (360-a-b)
-	bottom = 360-(2*theta)
-	return top/bottom
+    theta = math.acos(-1/3)*(180/math.pi)
+    top = (360-a-b)
+    bottom = 360-(2*theta)
+    return top/bottom
 
-def get_tau4(cif_file):
+def get_tau4(cif_file, central_atom):
+    try:
+        with open(cif_file,"r") as f:    
+            angles_lines = get_angle_lines(f.read(), central_atom)
+            f.close()
+    
+    except FileNotFoundError:
+        return None
 
-	try:
-		with open(cif_file,"r") as f:	
-			angles_lines = get_angle_lines(f.read(),central_atom)
+    if validate_angle_line_list(angles_lines) is not None:
+        angle_values = get_angle_values(angles_lines)
+        largest_2_angles = get_largest_2_angles(angle_values)
+        tau4 = calc_tau4(largest_2_angles[0], largest_2_angles[1])
 
-			f.close()
-	except FileNotFoundError:
-		return f"\nFile \"{cif_file}\" could not be found.\nCheck the spelling of the file name and path is all correct."
+    else:
+        explain_angle_list_issue(angles_lines, central_atom)
+        return None
 
-	if type(angles_lines) != list:
-
-		return angles_lines
-	else:
-		angle_values = get_angle_values(angles_lines)
-		largest_2_angles = get_largest_2_angles(angle_values)
-
-		tau4 = calc_tau4(largest_2_angles[0], largest_2_angles[1])
-		return tau4
+    return tau4
 
 try:
-	cif = sys.argv[1]
+    cif = sys.argv[1]
+    if (re.search(".cif", cif)):
+        pass
+    else:
+        print(f"File path \'{cif}\' does not contain a valid .cif file")
+        print("Exiting the program")
+        exit()
 except IndexError:
-	print("No file or central atom provided")
-	exit()
+    print("No file or central atom provided")
+    exit()
 
 try:
-	central_atom = sys.argv[2]
-except: IndexError:
-	print(f"Cannot calculate Tau4 for CIF \"{cif}\" because no central atom has been specificed")
-	exit()
+    central_atom = sys.argv[2]
+except IndexError:
+    print(f"Cannot calculate Tau4 for CIF \"{cif}\" because no central atom has been specificed")
+    exit()
 
-try:
-	print(f"Tau4 calculated at {central_atom} is {round(get_tau4(cif),2)}\n(To 2 decimal places)")
-except TypeError:
-	print(get_tau4(cif))
+tau4 = get_tau4(cif, central_atom)
+
+if tau4 is not None:
+    print(tau4)
+else:
+    print("Something went wrong")
+
+
